7d867d4e13df6ec85bb7ed496abbfd82f5272dd5
AStoyanoff <chessterfield@gmail.com>
2026-09-16 01:22:50 +0300

    Improved the UI with CSS style, HTML structure and JS effects

--- towns.js
+++ towns.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+    $('#btnDelete').click(deleteTown);
 });
 
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+    const townName = $('#townName').val();
+    $('#townName').val('');
+    let removed = false;
+
+    for (const option of $('#towns option')) {
+
+        if (option.textContent === townName) {
+            removed = true;
+            option.remove();
+        }
+    }
+
+    if (removed) {
+        showMessage(townName + ' deleted.');
+
+    } else {
+        showMessage(townName + ' not found.');
+    }
+}
+
+function showMessage(message) {
+    $('#result').text(message).css('display', 'block');
+
+    setTimeout(function() {
+        $('#result').hide('blind', {}, 500);
+
+    }, 3000);
 }
