f93c53c95dab9aa0a1fe8dd202c11063cc7f2108
AStoyanoff <chessterfield@gmail.com>
2026-09-16 01:02:38 +0300

    Implemented functionality to shuffle the existing towns

--- towns.js
+++ towns.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
+	$('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
 	let removed = false;
+	
 	for (let option of $('#towns option')) {
 		if (option.textContent == townName) {
 			removed = true;
@@ -16,4 +18,23 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function shuffleTowns() {
+    const towns = $('#towns option').toArray();
+
+    $('#towns').empty();
+    shuffleTownElements(towns);
+    $('#towns').append(towns);
+    $('#result').text('Towns shuffled.');
+
+    function shuffleTownElements(townElements) {
+        for (let currIdx = townElements.length - 1; currIdx > 0; currIdx--) {
+            let randomIdx = Math.floor(Math.random() * (currIdx + 1));
+            let oldElement = townElements[currIdx];
+
+            townElements[currIdx] = townElements[randomIdx];
+            townElements[randomIdx] = oldElement;
+        }
+    }
 }
